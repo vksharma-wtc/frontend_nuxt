cd184f0f1f5ef6960db92bdd86a471392bf7a5f7
Vivek Kumar Sharma <vivek.kumarsharma@vml.com>
2026-08-05 13:51:38 +0530

    added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/i18n', '@nuxtjs/tailwindcss'],
+  i18n: {
+    lazy: true,
+    langDir: 'locales',
+
+    defaultLocale: 'en',
+
+    strategy: 'prefix',
+
+    locales: [
+      {
+        code: 'en',
+        name: 'English',
+        file: 'en.json'
+      },
+      {
+        code: 'fr',
+        name: 'French',
+        file: 'fr.json'
+      },
+      {
+        code: 'nl',
+        name: 'Dutch',
+        file: 'nl.json'
+      }
+    ]
+  },
+
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
   },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
   runtimeConfig: {
     public: {
       wordpressGraphql:
